8de6ee2fc1be4c631b152d5f6bf4af18be07f953
Salma-Ponce <sa.gomezponce@ugto.mx>
2025-02-10 11:28:28 -0600

    mi primer programa

---
+++ Febrero10/primer_programa.c
@@ -0,0 +1,15 @@
+/*Creado el 10 de Febrero de 2025 por salma ponce*/
+
+#include<stdio.h>
+
+int main ()
+
+{
+
+	printf ("Hola Salma \n");
+	printf ("Este es el primer programa del curso\n");
+
+}
+
+/*Termine de escribir el programa*/
+
