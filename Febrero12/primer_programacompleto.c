54b40286cc3bf77d4f267c08903adde454547602
Salma-Ponce <sa.gomezponce@ugto.mx>
2025-02-12 12:01:10 -0600

    VARIABLES

---
+++ Febrero12/primer_programacompleto.c
@@ -0,0 +1,22 @@
+/*
+Febrero 12, 2024
+Primer programa completo, mostrando estructura, definicion de variables, imprimir y leer valores de variables en la pantalla.
+*/
+
+
+
+#include <stdlib.h> //libreria estandar de C.
+#include <stdio.h> //libreria para interaccion con la pantalla.
+
+int main (){ 
+	char nombre[10];
+	int edad;
+	float temperatura;
+	//Cuerpo del programa;
+	printf ("¿Cual es tu nombre? \Salma");
+	scanf("%s", nombre);
+	printf("Hola, %s este es el primer programa completo del curso PB2025\n",nombre); //el caracter "\n" indica un salto de linea.
+	
+	
+}
+
