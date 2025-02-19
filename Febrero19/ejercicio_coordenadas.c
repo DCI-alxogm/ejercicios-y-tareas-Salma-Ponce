2004037c504374f51affbf076e4f329b3294baca
Salma-Ponce <sa.gomezponce@ugto.mx>
2025-02-19 11:53:08 -0600

    EJERCICIO COORDENADAS

---
+++ Febrero19/ejercicio_coordenadas.c
@@ -0,0 +1,11 @@
+/*
+19 de Febrero de 2025 EJERCICIO COORDENADAS
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float x, y, x;	//Declaro x, y, z
+	//Cuerpo del programa
+	printf("Dame
